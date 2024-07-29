745e7815c59e5bb3e09a6c4f48bc12f04ebe3c8f
noshluk2 <noshluk2@gmail.com>
2024-07-29 17:40:21 +0500

    leaving drive_node.cpp

---
+++ robot_control_pkg/launch/tbsim_bringup.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    turtlesim_gui = Node(
+        package ='turtlesim',
+        executable = 'turtlesim_node',
+        name = 'turtlesim'
+    )
+
+    turtle_driver_node= Node(
+        package ='robot_control_pkg',
+        executable = 'drive_node',
+        name = 'turtle_driver',
+    )
+
+    return LaunchDescription([
+        turtlesim_gui,
+        turtle_driver_node
+    ])
